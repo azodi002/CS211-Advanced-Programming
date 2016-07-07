435fc6da4126f452a7bef92736fe6aa47bb6da79
Omid Azodi <azodi002@users.noreply.github.com>
2016-07-07 14:28:48 -0700

    Templates

---
+++ Template/Maps_Template.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+template <class T, class U>
+
+class CMap
+{
+private:
+  vector<T> keys;
+  vector<U> values;
+
+public:
+  void insert(T key, U value)
+  {
+  keys.push_back(key);
+  values.push_back(value);
+  }
+  
+  void get (int index)
+  {
+    cout << keys[index] << ": " << values[index];
+  }
+
+};
+
+
+int main(void)
+{
+
+  CMap <string, int> grades;
+  grades.insert("Jones", 88);
+  grades.insert("Smith", 90);
+  grades.get(0);
+  cout << endl;
+  grades.get(1);
+  cout << endl;
+
+
+
+
+
+  return 0;
+}
