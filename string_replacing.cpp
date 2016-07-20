c976cedb6cbdfe9b76b3043e0cae6ba381358a95
Omid Azodi <azodi002@users.noreply.github.com>
2016-07-20 15:18:45 -0700

    string_replacing

---
+++ string_replacing.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+  
+  string s1 = "a needle in a haystack";
+  string word = "needle";
+  
+  //Position of first occurence of word
+  int pos = s1.find(word);
+  
+  string s2 = s1.substr(pos, word.length());
+  cout << s2 << endl;
+  
+  string s3 = s1.substr(pos + word.length() + 1);
+  cout << s3 << endl;  
+
+  s1.replace(pos, word.length(), "pin");
+  cout << s1 << endl;
+
+
+
+  return 0;
+}
